be0fa4443e15b3a3906e6a551fe340611725af22
Piotr Usewicz <piotr@layer22.com>
2015-09-03 11:29:02 +0200

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-# Configure test features. Comment out the ones you don't want.
-
 require 'mocha/mini_test'                 # Mocha is a mocking and stubbing library for Ruby
 require 'ultimate-minitest-rails/support/minitest_reporters'      # Create customizable MiniTest output formats
 require 'ultimate-minitest-rails/support/database_rewinder'       # Minimalist's tiny and ultra-fast database cleaner
