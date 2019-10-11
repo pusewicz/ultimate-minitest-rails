b2de6623ceeb52c2798aebd1ff0d6278fba333a4
Piotr Usewicz <piotr@layer22.com>
2019-10-11 09:18:03 +0200

    Make support for Mocha the same

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,3 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-
-require 'mocha/mini_test'                 # Mocha is a mocking and stubbing library for Ruby
+require 'ultimate-minitest-rails/support/mocha'                   # Mocha is a mocking and stubbing library for Ruby
 require 'ultimate-minitest-rails/support/minitest_reporters'      # Create customizable MiniTest output formats
 require 'ultimate-minitest-rails/support/database_rewinder'       # Minimalist's tiny and ultra-fast database cleaner
-
-class ActiveSupport::TestCase
-  fixtures :all
-end
